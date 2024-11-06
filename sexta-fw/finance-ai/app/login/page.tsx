911fe408d581fcb89c937e441fce4878dbd45c02
Caio Ceretta <caioceretta@gmail.com>
2024-11-05 22:35:20 -0300

    feat: add authentication with clerk

--- sexta-fw/finance-ai/app/login/page.tsx
+++ sexta-fw/finance-ai/app/login/page.tsx
@@ -1,30 +1,42 @@
 import { Button } from "@/app/_components/ui/button";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import { LogInIcon } from "lucide-react";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  const { userId } = await auth()
+
+  if(userId) {
+    redirect('/')
+  }
+
   return (
     <div className="grid h-full grid-cols-2">
       {/* Left */}
       <div className="flex h-full flex-col justify-center p-8 max-w-[500px] mx-auto">
-      <Image alt="logo svg" width={173} height={89} src="/logo.svg" className="mb-8 " />
-      <h1 className="text-4xl font-bold mb-3">Bem vindo</h1>
-      <p className="text-muted-foreground mb-8">
-        A Finance AI é uma plataforma de gestão financeira que utiliza IA para monitorar suas movimentações, e oferecer
-        insights personalizados, facilitando o controle do seu orçamento
-      </p>
-      <Button variant={"outline"}>
-        <LogInIcon className="mr-2" />
-        Fazer Login
-      </Button>
+        <Image alt="logo svg" width={173} height={89} src="/logo.svg" className="mb-8 " />
+        <h1 className="text-4xl font-bold mb-3">Bem vindo</h1>
+        <p className="text-muted-foreground mb-8">
+          A Finance AI é uma plataforma de gestão financeira que utiliza IA para monitorar suas movimentações, e oferecer
+          insights personalizados, facilitando o controle do seu orçamento
+        </p>
+        <SignInButton>
+          <Button variant={"outline"}>
+            <LogInIcon className="mr-2" />
+            Fazer Login
+          </Button>
+        </SignInButton>
+
       </div>
 
       {/* Right*/}
       <div className="relative h-full w-full">
-      <Image alt="login image" fill src="/login.png" className="object-cover" />
+        <Image alt="login image" fill src="/login.png" className="object-cover" />
       </div>
     </div>
   );
 }
- 
+
 export default LoginPage;
