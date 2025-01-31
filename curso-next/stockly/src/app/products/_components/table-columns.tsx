cc755e64b83d717d8562603594dc46b4ea34da70
Caio Ceretta <caioceretta@gmail.com>
2025-01-31 00:08:17 -0300

    feat: up to products table and data table

---
+++ curso-next/stockly/src/app/products/_components/table-columns.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { Badge } from "@/app/_components/ui/badge";
+import type { Product } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import { CircleIcon } from "lucide-react";
+
+const getStatusLabel = (status: string) => {
+  if (status === "IN_STOCK") {
+    return "In Stock";
+  } else {
+    return "Out of Stock";
+  }
+};
+
+export const productTableColumns: ColumnDef<Product>[] = [
+  {
+    accessorKey: "name",
+    header: "Product",
+  },
+  {
+    accessorKey: "price",
+    header: "Unit Value",
+  },
+  {
+    accessorKey: "stock",
+    header: "Stock",
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: (row) => {
+      const product = row.row.original;
+      const label = getStatusLabel(product.status);
+
+      return (
+        <Badge
+          variant={label === "In Stock" ? "primary" : "destructive"}
+          className="gap-1.5"
+        >
+          <CircleIcon
+            size={14}
+            className={`${label === "In Stock" ? "fill-lime-950-foreground" : "fill-destructive-foreground"} }`}
+          />
+          {label}
+        </Badge>
+      );
+    },
+  },
+];
