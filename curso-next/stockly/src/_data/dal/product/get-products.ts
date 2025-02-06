48073feb12a44752f0bb81b0939febc979d182a4
Caio Ceretta <caioceretta@gmail.com>
2025-02-06 00:54:18 -0300

    feat: ISG on dal server actions

--- curso-next/stockly/src/_data/dal/product/get-products.ts
+++ curso-next/stockly/src/_data/dal/product/get-products.ts
@@ -2,7 +2,12 @@ import "server-only";
 
 import { db } from "@/_lib/prisma";
 import type { Product } from "@prisma/client";
+import { unstable_cache } from "next/cache";
 
 export const getProducts = async (): Promise<Product[]> => {
   return await db.product.findMany({});
 };
+
+export const cachedGetProducts = unstable_cache(getProducts, ["get-products"], {
+  revalidate: 5,
+});
--- curso-next/stockly/src/app/products/page.tsx
+++ curso-next/stockly/src/app/products/page.tsx
@@ -1,25 +1,62 @@
+import {
+  cachedGetProducts,
+  getProducts,
+} from "@/_data/dal/product/get-products";
 import { Button } from "@/app/_components/ui/button";
 import { PlusIcon } from "lucide-react";
 import { DataTable } from "../_components/ui/data-table";
 import { productTableColumns } from "./_components/table-columns";
-import ProductList from "./_components/product-list";
+import { unstable_cache } from "next/cache";
+
+/* 
+  By creating here like this, everytime the application is rebuilded, we are going to generate a new db connection, because
+  each PrismaClient is a connection with our db, so to avoid this, inside the folder lib and the file prisma.ts, we're
+  going to do the connection and call it inside where we'll use
+
+
+const prismaClient = new PrismaClient(); */
 
 const ProductsPage = async () => {
-  const baseUrl =
-    typeof window !== "undefined"
-      ? window.location.origin
-      : process.env.NEXT_PUBLIC_SITE_URL;
-
-  const response = await fetch(`${baseUrl}/api/products`);
-
-  if (!response.ok) {
-    throw new Error(`Error: ${response.status} ${response.statusText}`);
-  }
-
-  const { products } = await response.json();
+  const products = await cachedGetProducts();
+  const productsWithStatus = products.map((product) => ({
+    ...product,
+    status: product.stock > 0 ? "IN_STOCK" : "OUT_OF_STOCK",
+  }));
 
   return (
     <div className="ml-8 mt-8 w-full space-y-8 bg-white p-8 py-8">
+      {/*
+      Just a comment in case you forget
+
+      The reason why we have this div saying that the width is full, despite this being the default behavior of a div in
+      the HTML, is because there are these cases
+
+      1. Non-block parentes or with restricted width
+
+        - If this div is inside a container with the display:flex, inline-block or grid, its width may not be automatically
+        equal to 100% of the available width
+        - w-full ensure that it takes all the width of the parent element, even if the default behavior has been changed
+      
+      2. Restrictions inherited by ancestor elements
+
+        - If the parent of this div have a lower max-width or width, the behavior may vary
+        - w-full can ensure that it occupy all the width inside the limits imposed by the parent
+      
+      3. Flexbox may affect the width
+
+        - Since the display: flex has been applied, the children of this div may try to shrink according to the content,
+        depending on the applied properties
+        - w-full prevents this from happening and forces the div to remain occupying all the available space
+
+      4 Preventing unexpected stylings
+
+        - If there is any global rule or inherited CSS that define lower max-width or width to the div, w-full will
+        ensure that it expands correctly
+
+
+    */}
+
+      {/*/Left*/}
       <div className="flex w-full items-center justify-between">
         <div className="space-y-1">
           <span className="text-xs font-semibold text-slate-500">
@@ -32,10 +69,9 @@ const ProductsPage = async () => {
           New Product
         </Button>
       </div>
-      <ProductList />
       <DataTable
         columns={productTableColumns}
-        data={JSON.parse(JSON.stringify(products))}
+        data={JSON.parse(JSON.stringify(productsWithStatus))}
       />
     </div>
   );
