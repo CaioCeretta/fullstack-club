4bdba5cf01b0d8988aac872acc6447fd8cfec8ba
Caio Ceretta <caioceretta@gmail.com>
2025-02-19 15:07:11 -0300

    feat: separate the table-dropdown-menu from the table-columns and fixed typings

--- curso-next/stockly/src/app/products/_components/table-columns.tsx
+++ curso-next/stockly/src/app/products/_components/table-columns.tsx
@@ -1,32 +1,14 @@
 'use client'
 
-import {
-  AlertDialog,
-  AlertDialogTrigger,
-} from '@/app/_components/ui/alert-dialog'
 import { Badge } from '@/app/_components/ui/badge'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/app/_components/ui/dropdown-menu'
 import type { Product as PrismaProduct } from '@prisma/client'
 import { ColumnDef } from '@tanstack/react-table'
-import {
-  CircleIcon,
-  ClipboardCopyIcon,
-  EditIcon,
-  MoreHorizontalIcon,
-  TrashIcon,
-} from 'lucide-react'
-import DeleteProductDialogContent from './delete-product-dialog'
-import { Dialog, DialogTrigger } from '@/app/_components/ui/dialog'
-import UpsertProductDialogContent from './upsert-dialog-content'
-import { useState } from 'react'
+import { CircleIcon } from 'lucide-react'
+import { TableDropdownMenu } from './table-dropdown-menu'
 
-export interface Product extends PrismaProduct {
+export interface Product extends Omit<PrismaProduct, 'price'> {
   status: string
+  price: number
 }
 
 const getStatusLabel = (status: string) => {
@@ -82,53 +64,9 @@ export const productTableColumns: ColumnDef<Product>[] = [
     accessorKey: 'actions',
     header: 'Actions',
     cell: (row) => {
-      const [editDialogIsOpen, setEditDialogIsOpen] = useState(false)
       const product = row.row.original
-
-      return (
-        <AlertDialog>
-          <Dialog open={editDialogIsOpen} onOpenChange={setEditDialogIsOpen}>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <MoreHorizontalIcon size={16} />
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuItem
-                  className="gap-1.5"
-                  onClick={() => navigator.clipboard.writeText(product.id)}
-                >
-                  <ClipboardCopyIcon size={16} />
-                  Copy Id
-                </DropdownMenuItem>
-                <DialogTrigger>
-                  <DropdownMenuItem className="gap-1.5">
-                    <EditIcon size={16} />
-                    Edit
-                  </DropdownMenuItem>
-                </DialogTrigger>
-                <AlertDialogTrigger asChild>
-                  <DropdownMenuItem
-                    onSelect={(e) => e.preventDefault()} // Impede que o menu feche antes do AlertDialog abrir
-                  >
-                    <TrashIcon size={16} />
-                    Delete
-                  </DropdownMenuItem>
-                </AlertDialogTrigger>
-              </DropdownMenuContent>
-            </DropdownMenu>
-            <UpsertProductDialogContent
-              productValues={{
-                id: product.id,
-                name: product.name,
-                price: Number(product.price),
-                stock: product.stock,
-              }}
-              onDialogClose={() => setEditDialogIsOpen(false)}
-            />
-            <DeleteProductDialogContent productId={product.id} />
-          </Dialog>
-        </AlertDialog>
-      )
+      console.log(typeof product.price)
+      return <TableDropdownMenu product={product} />
     },
   },
 ]
---
+++ curso-next/stockly/src/app/products/_components/table-dropdown-menu.tsx
@@ -0,0 +1,76 @@
+'use client'
+
+import {
+  AlertDialog,
+  AlertDialogTrigger,
+} from '@/app/_components/ui/alert-dialog'
+import { Dialog, DialogTrigger } from '@/app/_components/ui/dialog'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/app/_components/ui/dropdown-menu'
+import {
+  ClipboardCopyIcon,
+  EditIcon,
+  MoreHorizontalIcon,
+  TrashIcon,
+} from 'lucide-react'
+import DeleteProductDialogContent from './delete-product-dialog'
+import UpsertDialogContent from './upsert-dialog-content'
+import type { UpsertProductType } from '@/app/_actions/product/upsert-product/schema'
+import { useState } from 'react'
+
+interface TableDropdownMenuProps {
+  product: UpsertProductType
+}
+
+export const TableDropdownMenu = ({ product }: TableDropdownMenuProps) => {
+  const [editDialogIsOpen, setEditDialogIsOpen] = useState(false)
+
+  return (
+    <AlertDialog>
+      <Dialog open={editDialogIsOpen} onOpenChange={setEditDialogIsOpen}>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <MoreHorizontalIcon size={16} />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem
+              className="gap-1.5"
+              onClick={() => navigator.clipboard.writeText(product.id!)}
+            >
+              <ClipboardCopyIcon size={16} />
+              Copy Id
+            </DropdownMenuItem>
+            <DialogTrigger>
+              <DropdownMenuItem className="gap-1.5">
+                <EditIcon size={16} />
+                Edit
+              </DropdownMenuItem>
+            </DialogTrigger>
+            <AlertDialogTrigger asChild>
+              <DropdownMenuItem
+                onSelect={(e) => e.preventDefault()} // Impede que o menu feche antes do AlertDialog abrir
+              >
+                <TrashIcon size={16} />
+                Delete
+              </DropdownMenuItem>
+            </AlertDialogTrigger>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <UpsertDialogContent
+          productValues={{
+            id: product.id,
+            name: product.name,
+            price: Number(product.price),
+            stock: product.stock,
+          }}
+          onDialogClose={() => setEditDialogIsOpen(false)}
+        />
+        <DeleteProductDialogContent productId={product.id!} />
+      </Dialog>
+    </AlertDialog>
+  )
+}
