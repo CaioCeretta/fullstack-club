d484db2850ddfec78a6a85638f5f772e74a16d0f
Caio Ceretta <caioceretta@gmail.com>
2025-02-20 00:55:53 -0300

    feat: sales component init

--- curso-next/stockly/src/app/sales/page.tsx
+++ curso-next/stockly/src/app/sales/page.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import { getProducts } from '@/_data/dal/product/get-products'
+import { Button } from '../_components/ui/button'
+import { Sheet, SheetTrigger } from '../_components/ui/sheet'
+import UpsertSheetContent from './_components/upsert-sheet-content'
 
-export interface SalesProps {}
+const SalesPage = async () => {
+  const products = await getProducts()
 
-export default function Sales() {
-  return <div>Sales Page</div>
+  return (
+    <div className="ml-8 mt-8 w-full space-y-8 bg-white p-8 py-8">
+      <div className="flex w-full items-center justify-between">
+        <div className="space-y-1">
+          <span className="text-xs font-semibold text-slate-500">
+            Sales Management
+          </span>
+          <h2 className="text-xl font-semibold">Sales</h2>
+        </div>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button>New Sale</Button>
+          </SheetTrigger>
+          <UpsertSheetContent />
+        </Sheet>
+      </div>
+    </div>
+  )
 }
+
+export default SalesPage
