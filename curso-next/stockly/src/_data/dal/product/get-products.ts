b989314e606d027fc4a9e1222bd4de59c1a7cb84
Caio Ceretta <caioceretta@gmail.com>
2025-03-15 23:30:59 -0300

    chore: product now calculates its status on the query

--- curso-next/stockly/src/_data/dal/product/get-products.ts
+++ curso-next/stockly/src/_data/dal/product/get-products.ts
@@ -6,8 +6,17 @@ import type { Product } from '@prisma/client'
 import { unstable_cache } from 'next/cache'
 // import { unstable_cache } from "next/cache";
 
-export const getProducts = async (): Promise<Product[]> => {
-  return await db.product.findMany({})
+export interface ProductDTO extends Product {
+  status: 'IN_STOCK' | 'OUT_OF_STOCK'
+}
+
+export const getProducts = async (): Promise<ProductDTO[]> => {
+  const products = await db.product.findMany({})
+
+  return products.map((product) => ({
+    ...product,
+    status: product.stock > 0 ? 'IN_STOCK' : 'OUT_OF_STOCK',
+  }))
 }
 
 // export const cachedGetProducts = unstable_cache(getProducts, ["get-products"], {
@@ -20,15 +29,3 @@ export const cachedGetProducts = unstable_cache(getProducts, ['getProducts'], {
   tags: ['get-products'],
   revalidate: 60,
 })
-
-export const cachedGetRandomNumber = unstable_cache(
-  async () => {
-    await new Promise((resolve) => setTimeout(resolve, 1000))
-    return Math.random()
-  },
-  ['getRandomNumber'],
-  {
-    tags: ['get-random-number '],
-    revalidate: 60,
-  },
-)
